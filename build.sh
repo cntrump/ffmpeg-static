f0a6828b836ccaed1c87fac7750c8a81917682a4
Lvv.me <cntrump@gmail.com>
2021-04-23 18:25:59 +0800

    ffmpeg 4.4

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-FFMPEG_VERSION=4.3.2
+FFMPEG_VERSION=4.4
 MIN_TARGET=10.9
+
+export MACOSX_DEPLOYMENT_TARGET=${MIN_TARGET}
+export CC=clang CXX=clang++
 
 brew update
 brew upgrade
@@ -29,6 +32,10 @@ fi
 git clone --depth=1 https://github.com/toots/shine.git
 cd ./shine
 ./bootstrap
+CC=clang \
+CFLAGS="-mmacosx-version-min=${MIN_TARGET}" \
+CPPFLAGS="-mmacosx-version-min=${MIN_TARGET}" \
+LDFLAGS="-mmacosx-version-min=${MIN_TARGET}" \
 ./configure --prefix=/usr/local --disable-shared
 make && make install
 
@@ -41,6 +48,10 @@ fi
 curl -O https://raw.githubusercontent.com/cntrump/build_ffmpeg_brew/master/vo-amrwbenc-0.1.3.tar.gz
 tar -zxvf ./vo-amrwbenc-0.1.3.tar.gz
 cd ./vo-amrwbenc-0.1.3
+CC=clang \
+CFLAGS="-mmacosx-version-min=${MIN_TARGET}" \
+CPPFLAGS="-mmacosx-version-min=${MIN_TARGET}" \
+LDFLAGS="-mmacosx-version-min=${MIN_TARGET}" \
 ./configure --prefix=/usr/local --disable-shared
 make && make install
 
@@ -53,7 +64,9 @@ fi
 curl -O https://raw.githubusercontent.com/cntrump/build_ffmpeg_brew/master/xavs.tar.bz2
 tar -jxvf ./xavs.tar.bz2
 cd ./xavs
-./configure --prefix=/usr/local --host=x86_64-darwin --disable-asm
+./configure --prefix=/usr/local --host=x86_64-darwin --disable-asm \
+            --extra-cflags="-mmacosx-version-min=${MIN_TARGET}" \
+            --extra-ldflags="-mmacosx-version-min=${MIN_TARGET}"
 make && make install
 
 cd ..
@@ -65,6 +78,10 @@ fi
 curl -O https://raw.githubusercontent.com/cntrump/build_ffmpeg_brew/master/zvbi-0.2.35.tar.bz2
 tar -jxvf ./zvbi-0.2.35.tar.bz2
 cd ./zvbi-0.2.35
+CC=clang \
+CFLAGS="-mmacosx-version-min=${MIN_TARGET}" \
+CPPFLAGS="-mmacosx-version-min=${MIN_TARGET}" \
+LDFLAGS="-mmacosx-version-min=${MIN_TARGET}" \
 ./configure --prefix=/usr/local --disable-shared
 make && make install
 
@@ -80,7 +97,7 @@ mkdir ./avisynth-build && cd ./avisynth-build
 cmake ../ -DHEADERS_ONLY:bool=on
 make install
 
-cd ..
+cd ../..
 
 if [ -d ./ffmpeg-${FFMPEG_VERSION} ]; then
   rm -rf ./ffmpeg-${FFMPEG_VERSION}
@@ -94,7 +111,12 @@ echo Building ffmpeg-${FFMPEG_VERSION} ...
 
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
 
-./configure --cc=/usr/bin/clang --prefix=/usr/local --extra-version=lvv.me --arch=x86_64 --enable-avisynth --enable-fontconfig --enable-gpl --enable-libaom --enable-libass --enable-libbluray --enable-libdav1d --enable-libfreetype --enable-libgsm --enable-libmodplug --enable-libmp3lame --enable-libmysofa --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libopenh264 --enable-libopenjpeg --enable-libopus --enable-librubberband --enable-libshine --enable-libsnappy --enable-libsoxr --enable-libspeex --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvmaf --enable-libvo-amrwbenc --enable-libvorbis --enable-libvpx --enable-libwavpack --enable-libwebp --enable-libx264 --enable-libx265 --enable-libxavs --enable-libxvid --enable-libzimg --enable-libzmq --enable-libzvbi --enable-version3 --pkg-config-flags=--static --disable-ffplay --enable-nonfree --enable-libfdk-aac --enable-openssl --extra-cflags="-I/usr/local/opt/openssl/include -mmacosx-version-min=${MIN_TARGET}" --extra-ldflags="-L/usr/local/opt/openssl/lib -mmacosx-version-min=${MIN_TARGET}"
+./configure --cc=${CC} --cxx=${CXX} --prefix=/usr/local --extra-version=lvv.me --arch=x86_64 \
+            --enable-avisynth --enable-fontconfig --enable-gpl --enable-libaom --enable-libass --enable-libbluray --enable-libdav1d --enable-libfreetype --enable-libgsm --enable-libmodplug --enable-libmp3lame --enable-libmysofa --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libopenh264 --enable-libopenjpeg --enable-libopus --enable-librubberband --enable-libshine --enable-libsnappy --enable-libsoxr --enable-libspeex --enable-libtheora --enable-libtwolame --enable-libvidstab --enable-libvmaf --enable-libvo-amrwbenc --enable-libvorbis --enable-libvpx --enable-libwebp --enable-libx264 --enable-libx265 --enable-libxavs --enable-libxvid --enable-libzimg --enable-libzmq --enable-libzvbi --enable-version3 --pkg-config-flags=--static --disable-ffplay \
+            --enable-nonfree --enable-libfdk-aac --enable-openssl \
+            --extra-cflags="-I/usr/local/opt/openssl/include -mmacosx-version-min=${MIN_TARGET}" \
+            --extra-cxxflags="-I/usr/local/opt/openssl/include -mmacosx-version-min=${MIN_TARGET}" \
+            --extra-ldflags="-L/usr/local/opt/openssl/lib -mmacosx-version-min=${MIN_TARGET}"
 
 make -j `sysctl -n hw.logicalcpu_max`
 make install
